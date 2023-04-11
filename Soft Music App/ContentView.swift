db5ff62c6bcbcddb1691e31e345983aff547c48a
Amish Tufail <amish.tufail2002@gmail.com>
2023-04-11 21:34:53 +0500

    Initial Commit

---
+++ Soft Music App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Soft Music App
+//
+//  Created by Amish Tufail on 11/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Soft Music App/Soft_Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soft_Music_AppApp.swift
+//  Soft Music App
+//
+//  Created by Amish Tufail on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Soft_Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
